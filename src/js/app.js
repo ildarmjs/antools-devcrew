cd5f36cd5cc9f9afedfe71543c2bf4bf5a9f7b35
Ildar <ildar.1795@gmail.com>
2023-01-26 20:07:51 +0400

    adpativ-styles

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,13 @@ isWebp()
 headerFixed()
 // ====================================================================================================================================================
 anchor()
+
+const iconMenu = document.querySelector('.icon-menu')
+const menuBody = document.querySelector('.menu__body')
+if (iconMenu) {
+	iconMenu.addEventListener('click', e => {
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active')
+		menuBody.classList.toggle('_active')
+	})
+}
